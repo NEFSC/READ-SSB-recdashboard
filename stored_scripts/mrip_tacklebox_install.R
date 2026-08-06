f9cdec7565f38e233b1ab1af3c6ac1b4c1c4c747
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-08-06 12:43:55 -0400

    comment out installation of mriptacklebox

--- stored_scripts/mrip_tacklebox_install.R
+++ stored_scripts/mrip_tacklebox_install.R
@@ -17,7 +17,7 @@ dir.create(file.path(tacklebox_main_lib ), showWarnings = FALSE)
 dir.create(file.path(tacklebox_dev_lib ), showWarnings = FALSE)
 
 #install main  branch of mriptacklebox
-remotes::install_github("NEFSC/READ-PDB-mriptacklebox",lib=tacklebox_main_lib , dependencies = TRUE, force = TRUE)
+#remotes::install_github("NEFSC/READ-PDB-mriptacklebox",lib=tacklebox_main_lib , dependencies = TRUE, force = TRUE)
 
 # Not run
 # sample code to in stall a install development branch of mriptacklebox
